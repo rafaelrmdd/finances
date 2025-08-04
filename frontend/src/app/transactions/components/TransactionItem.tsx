dc7efe40567e735221b8a1bc508c2887e58dd5b9
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-08-04 16:08:41 -0300

    add dynamic data and types for component TransactionItem

--- frontend/src/app/transactions/components/TransactionItem.tsx
+++ frontend/src/app/transactions/components/TransactionItem.tsx
@@ -1,12 +1,96 @@
-import { MdShoppingCart } from "react-icons/md";
+import { 
+  MdAccountBalanceWallet, 
+  MdKeyboardDoubleArrowUp, 
+  MdKeyboardDoubleArrowDown,
+  MdFilterList,
+  MdSearch,
+  MdAdd,
+  MdShoppingCart,
+  MdRestaurant,
+  MdLocalGasStation,
+  MdMovie,
+  MdHome,
+  MdSchool
+} from "react-icons/md";
 
-export function TransactionItem() {
+enum TransactionCategory {
+    INCOME = 'income',
+    FOOD = 'food',
+    TRANSPORTATION = 'transportation',
+    ENTERTAINMENT = 'entertainment',
+    HOUSING = 'housing',
+    EDUCATION = 'education',
+    OTHER = 'other',
+}
+
+//Remove the question marks when the backend is created '?'
+interface TransactionItemProps {
+    name?: string;
+    value?: string;
+    type?: 'income' | 'expense';
+    category?: TransactionCategory;
+    timestamp?: string;
+}
+
+export function TransactionItem({name, value, type, category, timestamp} : TransactionItemProps) {
+    //remove undefined when the backend is created
+    const handleIcon = (category: TransactionCategory | undefined) => {
+        switch(category) {
+            case TransactionCategory.INCOME:
+                return (
+                    <div className="p-3 rounded-full bg-green-200">
+                        <MdKeyboardDoubleArrowUp className="text-green-600 text-[1.5rem]" />
+                    </div>
+                )
+            case TransactionCategory.FOOD:
+                return (
+                    <div className="p-3 rounded-full bg-orange-200">
+                        <MdRestaurant className="text-orange-600 text-[1.5rem]" />
+                    </div>
+                );
+            case TransactionCategory.TRANSPORTATION:
+                return (
+                    <div className="p-3 rounded-full bg-blue-200">
+                        <MdLocalGasStation className="text-blue-600 text-[1.5rem]" />
+                    </div>
+                );
+            case TransactionCategory.ENTERTAINMENT:
+                return (
+                    <div className="p-3 rounded-full bg-purple-200">
+                        <MdMovie className="text-purple-600 text-[1.5rem]" />
+                    </div>
+                );
+            case TransactionCategory.HOUSING:
+                return (
+                    <div className="p-3 rounded-full bg-yellow-200">
+                        <MdHome className="text-yellow-600 text-[1.5rem]" />
+                    </div>
+                );
+            case TransactionCategory.EDUCATION:
+                return (
+                    <div className="p-3 rounded-full bg-indigo-200">
+                        <MdSchool className="text-indigo-600 text-[1.5rem]" />
+                    </div>
+                );
+            case TransactionCategory.OTHER:
+                return (
+                    <div className="p-3 rounded-full bg-gray-200">
+                        <MdShoppingCart className="text-gray-600 text-[1.5rem]" />
+                    </div>
+                );
+            default:
+                return (
+                    <div className="p-3 rounded-full bg-slate-200">
+                        <MdAccountBalanceWallet className="text-slate-600 text-[1.5rem]" />
+                    </div>
+                );
+        }
+    }
+
     return (
         <div className="flex justify-between bg-gray-700 rounded-lg p-4">
             <div className="flex w-[60%] gap-x-4">
-                <div className="p-3 rounded-full bg-green-200">
-                    <MdShoppingCart className="text-green-300 text-[1.5rem]"/>
-                </div>
+                {handleIcon(category)}
 
                 <div>
                     <h2 className="text-white font-semibold">Lorem, ipsum.</h2>
